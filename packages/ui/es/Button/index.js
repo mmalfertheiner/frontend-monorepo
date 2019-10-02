f0bc9d158827b7f7e1eda1762883c3dd365f0ba5
Martin Malfertheiner <martin.malfertheiner@gmail.com>
2019-10-02 15:43:58 +0200

    compile button

--- packages/ui/es/Button/index.js
+++ packages/ui/es/Button/index.js
@@ -1,7 +1,17 @@
+function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+
 import React from 'react';
+import styled from '@emotion/styled';
+const Button = styled.button`
+  &:hover {
+    color: green;
+  }
+`;
 export default (({
   children,
   ...props
 }) => {
-  return React.createElement("button", props, children);
+  return React.createElement(Button, _extends({
+    className: "m-10 p-10 rounded bg-teal-300 text-sm text-white hover:bg-teal-500"
+  }, props), children);
 });
