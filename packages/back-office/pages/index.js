e2c8583710820c6dcf4abb316ad2f162d31b1924
Martin Malfertheiner <martin.malfertheiner@gmail.com>
2019-10-11 07:57:09 +0200

    experiment with external lib

--- packages/back-office/pages/index.js
+++ packages/back-office/pages/index.js
@@ -1,88 +1,56 @@
-import React from 'react'
-import Head from 'next/head'
-import Nav from '../components/nav'
+import React from "react";
+import Head from "next/head";
 
-const Home = () => (
-  <div>
-    <Head>
-      <title>Home</title>
-      <link rel='icon' href='/static/favicon.ico' importance='low' />
-    </Head>
+import Form from "react-jsonschema-form";
 
-    <Nav />
+const schema = {
+  title: "Todo",
+  type: "object",
+  required: ["title"],
+  properties: {
+    title: { type: "string", title: "Title", default: "A new task" },
+    done: { type: "boolean", title: "Done?", default: false }
+  }
+};
 
-    <div className='hero'>
-      <h1 className='title'>Welcome to Next.js!</h1>
-      <p className='description'>
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
+const CustomTitleField = ({title, required, ...props}) => {
+  console.log(props)
+  const legend = required ? title + '*' : title;
+  return <div id="custom">{legend}</div>;
+};
 
-      <div className='row'>
-        <a href='https://nextjs.org/docs' className='card'>
-          <h3>Documentation &rarr;</h3>
-          <p>Learn more about Next.js in the documentation.</p>
-        </a>
-        <a href='https://nextjs.org/learn' className='card'>
-          <h3>Next.js Learn &rarr;</h3>
-          <p>Learn about Next.js by following an interactive tutorial!</p>
-        </a>
-        <a
-          href='https://github.com/zeit/next.js/tree/master/examples'
-          className='card'
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Find other example boilerplates on the Next.js GitHub.</p>
-        </a>
+const fields = {
+  TitleField: CustomTitleField
+};
+
+const uiSchema = {
+  title: {
+    classNames: "task-title foo-bar"
+  }
+};
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+        <link rel="icon" href="/static/favicon.ico" importance="low" />
+      </Head>
+
+      <div>
+        <h1 className="text-xl text-blue-500 text-center mt-10">Testing react json schema!</h1>
+
+        <Form
+          schema={schema}
+          uiSchema={uiSchema}
+          fields={fields}
+          onChange={() => console.log("changed")}
+          onSubmit={({formData}) => console.log("submitted", formData)}
+          onError={() => console.log("errors")}
+        />
       </div>
     </div>
+  );
+};
 
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
-  </div>
-)
-
-export default Home
+export default Home;
